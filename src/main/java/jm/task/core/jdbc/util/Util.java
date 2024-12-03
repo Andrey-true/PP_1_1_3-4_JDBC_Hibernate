42001eeb716eb39f12721ce9f3db15a5fe89d9e4
Andrey-true <andreymgn184123@gmail.com>
2024-12-03 22:47:44 +0500

    Задача решена. Исправил вывод на консоль.

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,10 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,6 +14,7 @@ public class Util {
     private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String USER = "postgres";
     private static final String PASWORD = "postgres";
+    private static SessionFactory sessionFactory;
 
     public static Connection getConnection () {
         try {
@@ -16,6 +22,34 @@ public class Util {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+
+    public static SessionFactory getSesionFactory() {
+
+
+        try {
+            Configuration configuration = new Configuration();
+
+            configuration.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver");
+            configuration.setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/postgres");
+            configuration.setProperty("hibernate.connection.username", "postgres");
+            configuration.setProperty("hibernate.connection.password", "postgres");
+            configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+            configuration.setProperty("hibernate.show_sql", "true");
+            configuration.setProperty("hibernate.current_session_context_class", "thread");
+            configuration.addAnnotatedClass(User.class);
+
+            StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties());
+            sessionFactory = configuration.buildSessionFactory(builder.build());
+
+
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+
+        return sessionFactory;
     }
 
 }
